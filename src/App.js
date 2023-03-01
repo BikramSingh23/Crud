9f842b90887a6a47cf76dbc63637eb9c7cb81bef
BikramSingh23 <126165348+BikramSingh23@users.noreply.github.com>
2023-03-01 14:02:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Reactcrud from './Crud/Reactcrud'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Reactcrud/>
+    </div>
+  )
+}
